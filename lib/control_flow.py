64219a4ed6bdec6bbc29cf2c2fb5090a5b228301
Jeffrey <jefbfitzp33@outlook.com>
2024-01-27 17:08:24 -0700

    lab complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-   if username.upper() == "ADMIN" and password == "12345":
-        return "Access granted"
-   else:
-        return "Access denied"
-
+    if username.upper() == "ADMIN" and password == "12345":
+        return 'Access granted'
+    else:
+        return 'Access denied'
+    
 
 def hows_the_weather(temperature):
-   if temperature > 85:
-        return "It's too dang hot out there!"
-   elif temperature > 65:
-        return "It's perfect out there!"
-   elif temperature > 40:
-        return "It's a little chilly out there!"
-   else: 
+    a=temperature
+    if a <40:
         return "It's brisk out there!"
-    
+    elif a <66:
+        return "It's a little chilly out there!"
+    elif a <86:
+        return"It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
+
     
 
 def fizzbuzz(num):
-    if num % 5 == 0 and num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
-    elif num % 5 == 0:
+    elif num % 5==0:
         return "Buzz"
-    elif num % 3 == 0:
+    elif num % 3==0:
         return "Fizz"
-    return num
+    else:
+        return num
+
 
 def calculator(operation, num1, num2):
-    if operation == "+":
+    a=operation
+    if a == '+':
         return num1 + num2
-    elif operation == "-":
+    elif a =="-":
         return num1 - num2
-    elif operation == "*":
+    elif a =="*":
         return num1 * num2
-    elif operation == "/":
-        return num1 / num2
+    elif a =="/":
+        return num1/num2
+    else:
+        print("Invalid operation!")
+        return None
 
-    print("Invalid operation!")
-    return None
-   
+#Write a function `calculator()` that takes three arguments: an operation and two
+#numbers. If the operation is one of the following: `+`, `-`, `*`, or `/`, return
+#the value of calling the operation on the two numbers. Otherwise, output a
+#message saying "Invalid operation!" and return `None`.
+
+
